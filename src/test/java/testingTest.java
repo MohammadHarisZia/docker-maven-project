5cfc159990d8f57185315b23f2722fd13939754e
Mohammad Haris Zia <63912301+MohammadHarisZia@users.noreply.github.com>
2022-12-09 13:10:07 +0500

    updated TestingTest.java

--- src/test/java/testingTest.java
+++ src/test/java/testingTest.java
@@ -7,25 +7,7 @@ class testingTest {
         assertEquals(4, testing.add(2, 2));
     }
 
-    @org.junit.jupiter.api.Test
-    void mul() {
-        assertAll(() -> assertEquals(4, testing.multiply(2, 2)),
-                () -> assertEquals(-4, testing.multiply(2, -2)),
-                () -> assertEquals(4, testing.multiply(-2, -2)),
-                () -> assertEquals(0, testing.multiply(1, 0)),
-                () -> assertEquals(0, testing.multiply(1, 0))
-
-        );
-    }
-
-    @org.junit.jupiter.api.Test
-    void div() {
-        assertAll(() -> assertEquals(1, testing.divide(2, 2)),
-                () -> assertEquals(-1, testing.divide(2, -2)),
-                () -> assertEquals(1, testing.divide(-2, -2))
-        );
-
-    }
+    
 
     @org.junit.jupiter.api.Test
     void sub() {
