a006d97b9baec18b0a2bf4012d04d802575cc55f
Mohammad Haris Zia <mhzxbox360@gmail.com>
2022-12-09 12:18:39 +0500

    first commit

---
+++ src/main/java/testing.java
@@ -0,0 +1,19 @@
+public class testing {
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+
+    public static int divide(int a, int b){
+        return a/b;
+    }
+
+    public static int minus(int a, int b){
+        return a-b;
+    }
+
+}
---
+++ src/test/java/testingTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class testingTest {
+
+    @org.junit.jupiter.api.Test
+    void sum() {
+        assertEquals(4, testing.add(2, 2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void mul() {
+        assertAll(() -> assertEquals(4, testing.multiply(2, 2)),
+                () -> assertEquals(-4, testing.multiply(2, -2)),
+                () -> assertEquals(4, testing.multiply(-2, -2)),
+                () -> assertEquals(0, testing.multiply(1, 0)),
+                () -> assertEquals(0, testing.multiply(1, 0))
+
+        );
+    }
+
+    @org.junit.jupiter.api.Test
+    void div() {
+        assertAll(() -> assertEquals(1, testing.divide(2, 2)),
+                () -> assertEquals(-1, testing.divide(2, -2)),
+                () -> assertEquals(1, testing.divide(-2, -2))
+        );
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void sub() {
+        assertEquals(0, testing.minus(2, 2));
+    }
+}
